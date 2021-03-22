4cbc51029def22005664257aae32d35ada9a8193
Rostislav Jadavan <rostislav.jadavan@avast.com>
2021-03-22 12:28:23 +0100

    init storage dir structure, error & not found pages fix

--- src/handlers/common.go
+++ src/handlers/common.go
@@ -10,8 +10,22 @@ var InvalidFilenameValidation string = "Invalid filename, valid examples: wiki_p
 
 func errorPage(err error, e *echo.Echo, c echo.Context) error {
 	e.Logger.Error(err)
-	tpl, _ := ui.Render(ui.TemplateError, map[string]interface{}{
+	tpl, err := ui.Render(ui.TemplateError, map[string]interface{}{
 		"Message": err.Error(),
 	})
+	if err != nil {
+		e.Logger.Error(err)
+	}
 	return c.HTML(http.StatusInternalServerError, tpl)
+}
+
+func notFoundPage(err error, e *echo.Echo, c echo.Context) error {
+	e.Logger.Warn(err)
+	tpl, err := ui.Render(ui.TemplateNotFound, map[string]interface{}{
+		"Message": err.Error(),
+	})
+	if err != nil {
+		e.Logger.Error(err)
+	}
+	return c.HTML(http.StatusNotFound, tpl)
 }
